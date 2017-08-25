e151fbf462f72db6868fcff11a704bcc24933b65
Alexander Martinz <alex@amartinz.at>
2017-08-25 21:14:01 +0200

    cheeseburger: assert on modem build timestamp
    Change-Id: Ifbcd24205a11efbecb0b55d4ddaf30178e245c28
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- device.mk
+++ device.mk
@@ -350,6 +350,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Recovery
+PRODUCT_PACKAGES += \
+    librecovery_updater_cheeseburger
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.1-impl \
