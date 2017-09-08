ffde7efb17f7769ad0649bf200297a1c8a2447ce
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-09-09 00:22:50 +0200

    cheeseburger: wifi: move WCNSS_qcom_cfg.ini to vendor
    Change-Id: I3929e7422bbfd25a4da8901df95a82c5acdfea73

--- device.mk
+++ device.mk
@@ -422,7 +422,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/hostapd.deny:system/etc/hostapd/hostapd.deny \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
