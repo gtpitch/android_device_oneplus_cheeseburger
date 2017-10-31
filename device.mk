fb8d844a7ec3d0c5584f5cfb329458e006760693
Brett Montague <bmontagu@gmail.com>
2017-10-31 18:56:39 +0000

    temp: bring back oppo/common
    Signed-off-by: Brett Montague <bmontagu@gmail.com>

--- device.mk
+++ device.mk
@@ -425,3 +425,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+$(call inherit-product, device/oppo/common/common.mk)
