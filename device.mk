adbcd6baea62b6f06c8a0be29a6c6e1d609e32f5
gtpitch <bmontagu@gmail.com>
2018-01-02 22:22:48 -0500

    cheeseburger: update camera configs from CAF
    Signed-off-by: gtpitch <bmontagu@gmail.com>

--- device.mk
+++ device.mk
@@ -164,16 +164,12 @@ PRODUCT_PACKAGES += \
     libbt-vendor \
     libbthost_if
 
-# Camera
-PRODUCT_PACKAGES += \
-    libcamera_shim \
-    SnapdragonCamera
-
+# Camera configuration file. Shared by passthrough/binderized camera HAL
 PRODUCT_PACKAGES += camera.device@3.2-impl
+PRODUCT_PACKAGES += camera.device@1.0-impl
 PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
-
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.camera.device@1.0
+# Enable binderized camera HAL
+PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-service
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
